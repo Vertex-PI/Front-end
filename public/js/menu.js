ad1b372a3ac0ba46a40f9264a6a10c091d80ffd2
GustavoRizerioDev <gustavo.rsouza@sptech.school>
2024-09-14 13:27:42 -0300

    :sparkles: feat: Aplicando funcionalidade de abrir e fechar o menu lateral, e funcionalidade de fechar ele ao clicar na tela fora dele

---
+++ public/js/menu.js
@@ -0,0 +1,17 @@
+const menuHamburguer = document.getElementById('menu-hamburguer');
+const menuLateral = document.getElementById('menu-lateral');
+const fecharMenu = document.getElementById('fechar-menu');
+
+menuHamburguer.addEventListener('click', function() {
+  menuLateral.classList.add('aberto');
+});
+
+fecharMenu.addEventListener('click', function() {
+  menuLateral.classList.remove('aberto');
+});
+
+document.addEventListener('click', function(event) {
+  if (!menuLateral.contains(event.target) && !menuHamburguer.contains(event.target)) {
+    menuLateral.classList.remove('aberto');
+  }
+});

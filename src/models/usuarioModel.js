249e65db398cfa3fcb938cf453c038cb7c3201f9
GustavoRizerioDev <gustavo.rsouza@sptech.school>
2024-09-27 16:58:04 -0300

    :sparkles: feat: Adição de models de cargos e usuarios

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -3,17 +3,17 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT idFuncionario, nomeFuncionario, emailFuncionario, fk_hotel as hotelId FROM funcionario WHERE emailFuncionario = '${email}' AND senha = '${senha}';
+        SELECT idUsuario, senha, email, fk_cargos as Cargo FROM Usuario WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function cadastrar(nome, email, senha, hotelId) {
+function cadastrar(nome,senha, email, cargoId) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
     
     var instrucaoSql = `
-        INSERT INTO funcionario (nomeFuncionario, emailFuncionario, senha, fk_hotel) VALUES ('${nome}', '${email}', '${senha}', '${hotelId}');
+        INSERT INTO Usuario(Nome, Senha, Email, fk_cargos) VALUES ('${nome}', '${email}', '${senha}', '${cargoId}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);

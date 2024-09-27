fcbba94461a7a14e95fbfbba804ad233922c0319
GustavoRizerioDev <gustavo.rsouza@sptech.school>
2024-09-27 16:58:48 -0300

    :sparkles: feat: Adição de rotas no arquivo principal

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var HOST_APP = process.env.APP_HOST;
 var app = express();
 
 var indexRouter = require("./src/routes/index");
-/* var usuarioRouter = require("./src/routes/usuarios"); */
+var usuarioRouter = require("./src/routes/usuarios"); 
+var hotelRouter = require("./src/routes/cargos");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,7 +24,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 app.use("/", indexRouter);
-/* app.use("/usuarios", usuarioRouter); */
+app.use("/usuarios", usuarioRouter); 
+app.use("/cargos", cargosRouter);
 
 
 app.listen(PORTA_APP, function () {

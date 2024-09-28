886d2fedb0a5f5601fe59f7e9f91e32cdc957b82
GustavoRizerioDev <gustavo.rsouza@sptech.school>
2024-09-27 23:10:00 -0300

    :sparkles: feat: Modificando model do usuario

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -9,11 +9,11 @@ function autenticar(email, senha) {
     return database.executar(instrucaoSql);
 }
 
-function cadastrar(nome,senha, email, cargoId) {
+function cadastrar(nome,senha, email, sexo, cargoId) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
     
     var instrucaoSql = `
-        INSERT INTO Usuario(Nome, Senha, Email, fk_cargos) VALUES ('${nome}', '${email}', '${senha}', '${cargoId}');
+        INSERT INTO Usuario(Nome, Senha, Email, Sexo, fk_cargos) VALUES ('${nome}', '${email}', '${senha}', '${sexo}', '${cargoId}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);

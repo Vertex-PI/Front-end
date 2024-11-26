ffdb0cc902b2b5584872250572029efa6cd670c2
GustavoRizerioDev <gustavo.rsouza@sptech.school>
2024-11-26 14:30:59 -0300

    :sparkles: feat: Adicionado rotas para o crud de usuarios

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -3,6 +3,14 @@ var router = express.Router();
 
 var usuarioController = require("../controllers/usuarioController");
 
+router.get("/listar", function (req, res) {
+    usuarioController.listar(req, res);
+});
+
+router.get("/listarPorUsuario/:idUsuario", function (req, res) {
+    usuarioController.listarPorUsuario(req, res);
+});
+
 router.post("/cadastrar", function (req, res) {
     usuarioController.cadastrar(req, res);
 })
@@ -17,6 +25,10 @@ router.put("/editar/:idUsuario", function (req, res) {
 
 router.put("/editarSenha/:idUsuario", function (req, res) {
     usuarioController.editarSenha(req, res);
+});
+
+router.put("/editarCargo/:idUsuario", function (req, res) {
+    usuarioController.editarCargo(req, res);
 });
 
 router.delete("/deletar/:idUsuario", function (req, res) {

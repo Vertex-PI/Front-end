47661860a2e892d91e12d587e0149b59fe5d2191
GustavoRizerioDev <gustavo.rsouza@sptech.school>
2024-11-30 02:47:26 -0300

    :sparkles: feat: Fazendo integração dos cruds no front end

--- public/js/cargos/editarCargos.js
+++ public/js/cargos/editarCargos.js
@@ -1,134 +1,191 @@
-function atualizarListaCargos() {
-    const container = document.querySelector(".div_cargos"); // Container para os cargos
-    container.innerHTML = `
-        <div class="cabecalho_cargos">
-            <span>ID</span>
-            <span>NOME</span>
-            <span>CARGO</span>
-        </div>
-    `;
-    fetch("/cargos/listar")
+let idCargoGlobal = null;
+
+
+function carregarCargos() {
+    fetch('/cargos/listarCargos')
         .then((res) => res.json())
         .then((cargos) => {
+            const container = document.getElementById('div_cargos');
+            container.innerHTML = ''; 
+
             cargos.forEach((cargo) => {
-                const divCargo = document.createElement("div");
-                divCargo.className = "cargo";
+                const divCargo = document.createElement('div');
+                divCargo.className = 'cargo';
+
                 divCargo.innerHTML = `
-                    <span>${cargo.idCargos}</span>
                     <span>${cargo.nome}</span>
-                    <span>${cargo.temPermissaoAdm}</span>
-                    <img src="../../img/img_dash/edit.svg" class="img_item" alt="Editar" onclick="editarCargo(${cargo.idCargos})">
-                    <img src="../../img/img_dash/trash-bin.svg" class="img_trash" alt="Excluir" onclick="deletarCargo(${cargo.idCargos})">
+                    <img src="../../img/img_dash/edit.svg" class="img_item" alt="Editar" onclick="modificarCargoPorNome(${cargo.idCargos}, '${cargo.nome}', '${cargo.permissao}')">
+                    <img src="../../img/img_dash/trash-bin.svg" class="img_trash" alt="Deletar" onclick="deletarCargo(${cargo.idCargos})">
                 `;
+
                 container.appendChild(divCargo);
             });
         })
-        .catch((err) => console.error("Erro ao carregar cargos:", err));
+        .catch((err) => console.error('Erro ao carregar cargos:', err));
 }
-function editarCargo(idCargo, nomeCargo) {
-    document.getElementById("div-modificar-cargo").style.display = "none";
-    document.getElementById("div-mudar-cargo").style.display = "block";
-    const inputCargo = document.getElementById("inp-editar-cargo-usuario");
-    inputCargo.value = nomeCargo; 
-    inputCargo.dataset.idCargo = idCargo;
+
+function modificarCargoPorNome(idCargo, nome, permissao) {
+
+    document.getElementById('div-modificar-cargo').style.display = 'none';
+
+    document.getElementById('div-editar-cargo').style.display = 'block';
+
+    document.getElementById('inp_nome_cargo_editar').value = nome;
+    document.getElementById('inp_permissao_cargo_editar').value = permissao;
+
+    idCargoGlobal = idCargo;
+
 }
-function salvarEdicaoCargo() {
-    const inputCargo = document.getElementById("inp-editar-cargo-usuario");
-    const idCargo = inputCargo.dataset.idCargo;
-    const novoNomeCargo = inputCargo.value;
-    if (!novoNomeCargo) {
+
+function salvarEdicaoCargoPorNome() {
+    const nomeCargoInput = document.getElementById('inp_nome_cargo_editar');
+    const permissaoCargoInput = document.getElementById('inp_permissao_cargo_editar');
+
+    let nomeCargo = nomeCargoInput.value;
+    let permissaoCargo = permissaoCargoInput.value;
+
+    if (!nomeCargo) {
+        nomeCargo = nomeCargoInput.defaultValue;
+    }
+
+    if (!permissaoCargo) {
+        permissaoCargo = permissaoCargoInput.defaultValue;
+    }
+
+    if (!nomeCargo || !permissaoCargo) {
         Swal.fire({
-            icon: "warning",
-            title: "Atenção",
-            text: "Por favor, insira um nome válido para o cargo.",
+            icon: 'warning',
+            title: 'Atenção',
+            text: 'Todos os campos precisam ser preenchidos!',
+            backdrop: false,
         });
         return;
     }
-    fetch(`/cargos/editar/${idCargo}`, {
-        method: "PUT",
+
+    fetch(`/cargos/editar/${idCargoGlobal}`, {
+        method: 'PUT',
         headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ nome: novoNomeCargo }),
+        body: JSON.stringify({ 
+            nomeCargo: nomeCargo, 
+            permissao: permissaoCargo 
+        }),
     })
         .then((res) => {
             if (res.ok) {
                 Swal.fire({
-                    icon: "success",
-                    title: "Sucesso",
-                    text: "Cargo atualizado com sucesso!",
+                    icon: 'success',
+                    title: 'Sucesso',
+                    text: 'Cargo editado com sucesso!',
+                    backdrop: false,
                 }).then(() => {
-                    document.getElementById("div-mudar-cargo").style.display = "none";
-                    listarCargos(); // Atualiza a lista de cargos
+                    document.getElementById('div-editar-cargo').style.display = 'none';
+
+                    document.getElementById('div-modificar-cargo').style.display = 'flex';
+
+                    carregarCargos();
                 });
             } else {
                 Swal.fire({
-                    icon: "error",
-                    title: "Erro",
-                    text: "Houve um problema ao atualizar o cargo.",
+                    icon: 'error',
+                    title: 'Erro',
+                    text: 'Erro ao editar o cargo!',
+                    backdrop: false,
                 });
             }
         })
         .catch((err) => {
-            console.error("Erro ao atualizar cargo:", err);
+            console.error('Erro ao editar cargo:', err);
             Swal.fire({
-                icon: "error",
-                title: "Erro",
-                text: "Erro de conexão com o servidor. Tente novamente mais tarde.",
+                icon: 'error',
+                title: 'Erro',
+                text: 'Erro de conexão com o servidor. Tente novamente mais tarde.',
+                backdrop: false,
             });
         });
 }
-function deletarCargo(idCargo) {
-    if (!idCargo) {
-        Swal.fire({
-            icon: "error",
-            title: "Erro",
-            text: "ID do cargo inválido!",
-        });
-        return;
-    }
-    Swal.fire({
-        title: "Tem certeza?",
-        text: `Deseja deletar este cargo?`,
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Sim, deletar",
-        cancelButtonText: "Cancelar",
-    }).then((result) => {
-        if (result.isConfirmed) {
-            fetch(`/cargos/deletar/${idCargo}`, {
-                method: "DELETE",
-            })
-                .then((res) => {
-                    if (res.ok) {
-                        Swal.fire({
-                            icon: "success",
-                            title: "Sucesso",
-                            text: "Cargo deletado com sucesso!",
-                        });
-                        listarCargos(); // Atualiza a lista de cargos
-                    } else {
-                        Swal.fire({
-                            icon: "error",
-                            title: "Erro",
-                            text: "Erro ao deletar o cargo.",
-                        });
-                    }
-                })
-                .catch((err) => {
-                    console.error("Erro ao deletar cargo:", err);
-                    Swal.fire({
-                        icon: "error",
-                        title: "Erro",
-                        text: "Erro de conexão com o servidor. Tente novamente mais tarde.",
-                    });
+
+function deletarCargo(idCargos) {
+    console.log(`Deletando cargo para ID - ${idCargos}`);
+
+    fetch(`/cargos/verificarAssociacao/${idCargos}`)
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.associado) {
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'Atenção',
+                    text: 'Este cargo está associado a um usuário e não pode ser deletado!',
+                    backdrop: false,
                 });
-        }
-    });
+            } else {
+                Swal.fire({
+                    title: 'Tem certeza?',
+                    text: 'Deseja deletar este cargo?',
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Sim, deletar',
+                    cancelButtonText: 'Cancelar',
+                    backdrop: false,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        fetch(`/cargos/deletar/${idCargos}`, {
+                            method: 'DELETE',
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                        })
+                            .then((res) => {
+                                if (res.ok) {
+                                    Swal.fire({
+                                        icon: 'success',
+                                        title: 'Sucesso',
+                                        text: 'Cargo deletado com sucesso!',
+                                        backdrop: false,
+                                    }).then(() => {
+                                        carregarCargos(); 
+                                    });
+                                } else {
+                                    Swal.fire({
+                                        icon: 'error',
+                                        title: 'Erro',
+                                        text: 'Este cargo está associado a um usuário e não pode ser deletado!',
+                                        backdrop: false,
+                                    });
+                                }
+                            })
+                            .catch((err) => {
+                                console.error('Erro ao deletar cargo:', err);
+                                Swal.fire({
+                                    icon: 'error',
+                                    title: 'Erro',
+                                    text: 'Erro de conexão com o servidor. Tente novamente mais tarde.',
+                                    backdrop: false,
+                                });
+                            });
+                    }
+                });
+            }
+        })
+        .catch((err) => {
+            console.error('Erro ao verificar associação do cargo:', err);
+            Swal.fire({
+                icon: 'error',
+                title: 'Erro',
+                text: 'Erro ao verificar se o cargo está associado.',
+                backdrop: false,
+            });
+        });
 }
-document.addEventListener("DOMContentLoaded", () => {
-    listarCargos();
-});
-document.querySelector(".btn_adc_cadastro").addEventListener("click", salvarEdicaoCargo);
+
+
+function cancelarEdicaoCargo() {
+    document.getElementById('div-editar-cargo').style.display = 'none';
+
+    document.getElementById('div-modificar-cargo').style.display = 'flex';
+}
+
+document.addEventListener('DOMContentLoaded', carregarCargos);

5ff7602ab9ce2d5260451c01c0cfcce80cfa6574
Gustavo Rizerio <160532303+GustavoRizerioDev@users.noreply.github.com>
2024-12-04 15:07:13 -0300

    Update kpiModel.js

--- src/models/kpiModel.js
+++ src/models/kpiModel.js
@@ -18,20 +18,25 @@ WHERE
 
   function buscarComparacaoMesAtual() {
     var instrucaoSql = `
- SELECT
+    SELECT
     COUNT(*) AS totalMetasAtingidas
 FROM (
     SELECT
         m.mes,
         SUM(e.gastoEnergetico) AS totalEnergiaConsumida,
-        SUM(m.gastoEnergetico) AS totalMetaEnergia
-    FROM Metas m
-    JOIN Energia e ON e.fk_empresa = m.fk_empresa
-    WHERE e.fk_empresa = 1
-    AND e.ano = YEAR(CURDATE())
-    AND e.mes = m.mes
-    GROUP BY m.mes
-    HAVING totalEnergiaConsumida <= totalMetaEnergia
+        m.gastoEnergetico AS totalMetaEnergia
+    FROM
+        Metas m
+    JOIN
+        Energia e ON e.fk_empresa = m.fk_empresa
+                   AND e.mes = m.mes
+    WHERE
+        e.fk_empresa = 1
+        AND e.ano = YEAR(CURDATE())
+    GROUP BY
+        m.mes, m.gastoEnergetico
+    HAVING
+        totalEnergiaConsumida <= totalMetaEnergia
 ) AS metasAtingidas;  
     `;
   
@@ -41,12 +46,21 @@ FROM (
 
   function buscarMetaAtingida() {
     var instrucaoSql = `
-  SELECT
-        ROUND((SUM(e.gastoEnergetico) / SUM(m.gastoEnergetico)) * 100, 2) AS porcentagemAtingida
-    FROM Energia e
-    JOIN Metas m ON e.fk_empresa = m.fk_empresa AND e.mes = m.mes
-    WHERE e.fk_empresa = 1
-    AND e.ano = YEAR(CURDATE())
+SELECT
+    ROUND(SUM(CASE WHEN e.ano = YEAR(CURDATE()) THEN e.gastoEnergetico ELSE 0 END), 2) AS totalAnoAtual,
+    ROUND(SUM(CASE WHEN e.ano = YEAR(CURDATE()) - 1 THEN e.gastoEnergetico ELSE 0 END), 2) AS totalAnoAnterior,
+    ROUND(
+        (
+            (SUM(CASE WHEN e.ano = YEAR(CURDATE()) THEN e.gastoEnergetico ELSE 0 END) - 
+             SUM(CASE WHEN e.ano = YEAR(CURDATE()) - 1 THEN e.gastoEnergetico ELSE 0 END))
+            / NULLIF(SUM(CASE WHEN e.ano = YEAR(CURDATE()) - 1 THEN e.gastoEnergetico ELSE 0 END), 0)
+        ) * 100, 2
+    ) AS porcentagemAtingida
+FROM
+    Energia e
+WHERE
+    e.fk_empresa = 1
+    AND e.ano IN (YEAR(CURDATE()), YEAR(CURDATE()) - 1);
     `;
   
     console.log("Executando a instrução SQL para buscar porcentagem da meta atingida: \n" + instrucaoSql);
